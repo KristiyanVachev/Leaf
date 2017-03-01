1030b29ec1c7093119078fa9f1177e302133ab73
KristiyanVachev <typhon04@gmail.com>
2017-03-01 17:44:13 +0200

    Created Web client

---
+++ src/Leaf.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Leaf.Web.Startup))]
+namespace Leaf.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
