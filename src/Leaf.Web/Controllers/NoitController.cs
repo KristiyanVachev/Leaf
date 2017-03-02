6be976089a969bdbbf6adfc0b2dcc2414d40568b
KristiyanVachev <typhon04@gmail.com>
2017-03-02 12:22:04 +0200

    Added NoitController with views

---
+++ src/Leaf.Web/Controllers/NoitController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Leaf.Web.Controllers
+{
+    public class NoitController : Controller
+    {
+        // GET: Noit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Training()
+        {
+            return View();
+        }
+    }
+}   
