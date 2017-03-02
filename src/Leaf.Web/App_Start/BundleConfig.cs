d7eb34b06dbb08098e4fab610226e9591d941ffe
KristiyanVachev <typhon04@gmail.com>
2017-03-02 02:50:39 +0200

    Added theme

--- src/Leaf.Web/App_Start/BundleConfig.cs
+++ src/Leaf.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Leaf.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.superhero.min.css",
                       "~/Content/site.css"));
         }
     }
