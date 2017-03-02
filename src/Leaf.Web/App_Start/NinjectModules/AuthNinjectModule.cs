71758097fa22e6dcf25f97fa6fa1d9f326a3274e
KristiyanVachev <typhon04@gmail.com>
2017-03-02 03:15:08 +0200

    Added Ninject

---
+++ src/Leaf.Web/App_Start/NinjectModules/AuthNinjectModule.cs
@@ -0,0 +1,14 @@
+﻿using Leaf.Auth;
+using Leaf.Auth.Contracts;
+using Ninject.Modules;
+
+namespace Leaf.Web.App_Start.NinjectModules
+{
+    public class AuthNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IAuthenticationProvider>().To<HttpContextAuthenticationProvider>();
+        }
+    }
+}
---
+++ src/Leaf.Web/App_Start/NinjectModules/DataNinjectModule.cs
@@ -0,0 +1,17 @@
+﻿using Leaf.Data;
+using Leaf.Data.Contracts;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace Leaf.Web.App_Start.NinjectModules
+{
+    public class DataNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<ILeafDbContext>().To<LeafDbContext>().InRequestScope();
+            this.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
+            this.Bind(typeof(IRepository<>)).To(typeof(Repository<>)).InRequestScope();
+        }
+    }
+}
