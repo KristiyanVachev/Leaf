21098c00013341c933d3770b3c36b6d4f2edbd42
KristiyanVachev <typhon04@gmail.com>
2017-03-05 21:28:05 +0200

    Added question partial view

--- src/Leaf.Web/Controllers/NoitController.cs
+++ src/Leaf.Web/Controllers/NoitController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using Bytes2you.Validation;
+using Leaf.Models;
 using Leaf.Services.Contracts;
 using Leaf.Services.Noit;
 
@@ -33,9 +34,22 @@ namespace Leaf.Web.Controllers
             //create a fullTest
             //collect all questions
 
+            //Show start test button
+
             var questions = this.fullGameService.GetQuestions();
 
             return View(questions);
+
         }
+
+        //Something to be called with AJAX and containing the given answer and preferbly the question ID,
+        //Then it would pass the same partial view with the next question
+        public ActionResult NextQuestion(Question question)
+        {
+
+
+            return PartialView("_Question", question);
+        }
+
     }
 }
