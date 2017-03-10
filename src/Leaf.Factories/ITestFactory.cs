3c3d9e0521e9fa9fa083725d444c54ab9c49fc8d
KristiyanVachev <typhon04@gmail.com>
2017-03-10 12:43:20 +0200

    Added factories

---
+++ src/Leaf.Factories/ITestFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Leaf.Models;
+
+namespace Leaf.Factories
+{
+    public interface ITestFactory
+    {
+        Test CreateTest(string userId, IEnumerable<Question> questions, bool isFinished = false);
+    }
+}
---
+++ src/Leaf.Web/App_Start/NinjectModules/FactoryNinjectModule.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using Leaf.Factories;
+using Ninject.Extensions.Factory;
+
+namespace Leaf.Web.App_Start.NinjectModules
+{
+    public class FactoryNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<ITestFactory>().ToFactory().InSingletonScope();
+        }
+    }
+}
