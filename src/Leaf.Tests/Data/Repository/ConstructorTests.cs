9e1a77fd62fcf07ed44fcf3c59e10ee182af87e2
KristiyanVachev <typhon04@gmail.com>
2017-03-13 20:57:50 +0200

    Added Leaf.Tests project

---
+++ src/Leaf.Tests/Data/Repository/ConstructorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Leaf.Data;
+using Leaf.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Data.Repository
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void Constructor_PassDbContextNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new Repository< FakeRepositoryType > (null));
+        }
+
+        [Test]
+        public void Constructor_PassDbContextCorrectly_ShouldNotThrow()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILeafDbContext>();
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => new Repository<FakeRepositoryType>(mockedDbContext.Object));
+        }
+
+        [Test]
+        public void Constructor_PassDbContextCorrectly_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILeafDbContext>();
+
+            // Act
+            var repository = new Repository<FakeRepositoryType>(mockedDbContext.Object);
+
+            // Assert
+            Assert.IsNotNull(repository);
+        }
+    }
+
+    public class FakeRepositoryType
+    {
+        
+    }
+}
