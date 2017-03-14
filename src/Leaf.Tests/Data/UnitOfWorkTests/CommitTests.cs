7b7c45254b424bce9dd2bc3493e7339696df64af
KristiyanVachev <typhon04@gmail.com>
2017-03-14 16:40:17 +0200

    Added CommitTests

---
+++ src/Leaf.Tests/Data/UnitOfWorkTests/CommitTests.cs
@@ -0,0 +1,26 @@
+﻿using Leaf.Data;
+using Leaf.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Data.UnitOfWorkTests
+{
+    [TestFixture]
+    public class CommitTests
+    {
+        [Test]
+        public void TestCommit_ShouldCallDbContextSaveChanges()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILeafDbContext>();
+
+            var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+            // Act
+            unitOfWork.Commit();
+
+            // Assert
+            mockedDbContext.Verify(c => c.SaveChanges(), Times.Once);
+        }
+    }
+}
