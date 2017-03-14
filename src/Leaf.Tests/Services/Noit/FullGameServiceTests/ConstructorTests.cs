5ac9b355195842cec9da40dd8079bb54b1424ee8
KristiyanVachev <typhon04@gmail.com>
2017-03-15 01:07:25 +0200

    Added service test

---
+++ src/Leaf.Tests/Services/Noit/FullGameServiceTests/ConstructorTests.cs
@@ -0,0 +1,38 @@
+﻿using Leaf.Data.Contracts;
+using Leaf.Factories;
+using Leaf.Models;
+using Leaf.Services.Noit;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Services.Noit.FullGameServiceTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            //Arrange
+            var mockQuestionRepository = new Mock<IRepository<Question>>();
+            var mockAnswerRepository = new Mock<IRepository<Answer>>();
+            var mockCategoryRepository = new Mock<IRepository<Category>>();
+            var mockTestRepository = new Mock<IRepository<Test>>();
+            var mockAnsweredQuestionRepository = new Mock<IRepository<AnsweredQuestion>>();
+            var mockTestFactory = new Mock<ITestFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            //Act && Assert
+            Assert.DoesNotThrow(() =>
+                new FullGameService(mockQuestionRepository.Object,
+                    mockAnswerRepository.Object,
+                    mockCategoryRepository.Object,
+                    mockTestRepository.Object,
+                    mockAnsweredQuestionRepository.Object,
+                    mockTestFactory.Object,
+                    mockUnitOfWork.Object
+                    )
+            );
+        }
+    }
+}
