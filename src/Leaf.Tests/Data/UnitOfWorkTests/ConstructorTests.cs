0352bf5dd0673fdbf71b1d3ec5a48621afa5a2d7
KristiyanVachev <typhon04@gmail.com>
2017-03-14 16:36:32 +0200

    Changed Repository test folder name and added UnitOfWork tests

---
+++ src/Leaf.Tests/Data/UnitOfWorkTests/ConstructorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Leaf.Data;
+using Leaf.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Data.UnitOfWorkTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassDbContextNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new UnitOfWork(null));
+        }
+
+        [Test]
+        public void TestConstructor_PassDbContextCorrectly_ShouldNotThrow()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILeafDbContext>();
+
+            // Act, Assert
+            Assert.DoesNotThrow(() => new UnitOfWork(mockedDbContext.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassDbContextCorrectly_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<ILeafDbContext>();
+
+            // Act
+            var unitOfWork = new UnitOfWork(mockedDbContext.Object);
+
+            // Assert
+            Assert.IsNotNull(unitOfWork);
+        }
+    }
+}
