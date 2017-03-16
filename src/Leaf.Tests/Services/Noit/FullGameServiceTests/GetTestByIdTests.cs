e493ee7fc7f4c26da0fc9347a5750e23bab8289c
KristiyanVachev <typhon04@gmail.com>
2017-03-16 15:00:21 +0200

    Added FullGameService tests

---
+++ src/Leaf.Tests/Services/Noit/FullGameServiceTests/GetTestByIdTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Leaf.Data.Contracts;
+using Leaf.Factories;
+using Leaf.Models;
+using Leaf.Services.Noit;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Services.Noit.FullGameServiceTests
+{
+    [TestFixture]
+    public class GetTestByIdTests
+    {
+        [TestCase(4)]
+        [TestCase(53242)]
+        public void GetTestById_ShouldCallTestRepositoryGetById(int id)
+        {
+            //Arrange
+            var mockQuestionRepository = new Mock<IRepository<Question>>();
+            var mockAnswerRepository = new Mock<IRepository<Answer>>();
+            var mockCategoryRepository = new Mock<IRepository<Category>>();
+
+            var mockTest = new Mock<Test>();
+            mockTest.Setup(x => x.Questions).Returns(new List<Question>());
+            
+            var mockTestRepository = new Mock<IRepository<Test>>();
+            mockTestRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(mockTest.Object);
+
+            var mockAnsweredQuestionRepository = new Mock<IRepository<AnsweredQuestion>>();
+            var mockTestFactory = new Mock<ITestFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var service = new FullGameService(mockQuestionRepository.Object,
+                mockAnswerRepository.Object,
+                mockCategoryRepository.Object,
+                mockTestRepository.Object,
+                mockAnsweredQuestionRepository.Object,
+                mockTestFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+            //Act 
+            service.GetTestById(id);
+
+            //Assert
+            mockTestRepository.Verify(x => x.GetById(id), Times.Once);
+        }
+    }
+}
---
+++ src/Leaf.Tests/Services/Noit/FullGameServiceTests/HasUnfinishedTestTests.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using Leaf.Data.Contracts;
+using Leaf.Factories;
+using Leaf.Models;
+using Leaf.Services.Noit;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Services.Noit.FullGameServiceTests
+{
+    [TestFixture]
+    public class HasUnfinishedTestTests
+    {
+        [TestCase("4")]
+        [TestCase("ndasfsa")]
+        public void GetTestById_ShouldCallTestRepository(string userId)
+        {
+            //Arrange
+            var mockQuestionRepository = new Mock<IRepository<Question>>();
+            var mockAnswerRepository = new Mock<IRepository<Answer>>();
+            var mockCategoryRepository = new Mock<IRepository<Category>>();
+
+            var fakeTest = new Test();
+
+            var mockTestRepository = new Mock<IRepository<Test>>();
+            mockTestRepository.Setup(x => x.Entities).Returns(new List<Test> { fakeTest });
+
+            var mockAnsweredQuestionRepository = new Mock<IRepository<AnsweredQuestion>>();
+            var mockTestFactory = new Mock<ITestFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var service = new FullGameService(mockQuestionRepository.Object,
+                mockAnswerRepository.Object,
+                mockCategoryRepository.Object,
+                mockTestRepository.Object,
+                mockAnsweredQuestionRepository.Object,
+                mockTestFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+            //Act 
+            service.HasUnfinishedTest(userId);
+
+            //Assert
+            mockTestRepository.Verify(x => x.Entities, Times.Once);
+        }
+
+        [TestCase("4")]
+        [TestCase("ndasfsa")]
+        public void GetTestById_ShouldReturnFalse_WhenNoUserTestIsFound(string userId)
+        {
+            //Arrange
+            var mockQuestionRepository = new Mock<IRepository<Question>>();
+            var mockAnswerRepository = new Mock<IRepository<Answer>>();
+            var mockCategoryRepository = new Mock<IRepository<Category>>();
+
+            var fakeTest = new Test
+            {
+                UserId = userId + "something else",
+            };
+
+            var mockTestRepository = new Mock<IRepository<Test>>();
+            mockTestRepository.Setup(x => x.Entities).Returns(new List<Test> { fakeTest });
+
+            var mockAnsweredQuestionRepository = new Mock<IRepository<AnsweredQuestion>>();
+            var mockTestFactory = new Mock<ITestFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var service = new FullGameService(mockQuestionRepository.Object,
+                mockAnswerRepository.Object,
+                mockCategoryRepository.Object,
+                mockTestRepository.Object,
+                mockAnsweredQuestionRepository.Object,
+                mockTestFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+            //Act 
+            var result = service.HasUnfinishedTest(userId);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
