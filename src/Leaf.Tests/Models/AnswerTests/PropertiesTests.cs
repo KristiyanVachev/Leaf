53616ead4a6ddb3fbc98a1e45cfb08edb8240a68
KristiyanVachev <typhon04@gmail.com>
2017-03-18 10:35:05 +0200

    Added Answer tests

---
+++ src/Leaf.Tests/Models/AnswerTests/PropertiesTests.cs
@@ -0,0 +1,65 @@
+﻿using Leaf.Models;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Models.AnswerTests
+{
+    [TestFixture]
+    public class PropertiesTests
+    {
+        [TestCase("What is the purpose of this test?")]
+        [TestCase("Nana-nana-na-nananana")]
+        public void Content_ShouldSetCorrectly(string value)
+        {
+            //Arrange
+            var answer = new Answer();
+
+            //Act
+            answer.Content = value;
+
+            //Assert
+            Assert.AreEqual(value, answer.Content);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsCorrect_ShouldSetCorrectly(bool value)
+        {
+            //Arrange
+            var answer = new Answer();
+
+            //Act
+            answer.IsCorrect = value;
+
+            //Assert
+            Assert.AreEqual(value, answer.IsCorrect);
+        }
+
+        [TestCase(2)]
+        [TestCase(423)]
+        public void QuestionId_ShouldSetCorrectly(int value)
+        {
+            //Arrange
+            var answer = new Answer();
+
+            //Act
+            answer.QuestionId = value;
+
+            //Assert
+            Assert.AreEqual(value, answer.QuestionId);
+        }
+
+        [Test]
+        public void Questions_ShouldSetCorrectly()
+        {
+            //Arrange
+            var fakeQuestions = new Question();
+            var answer = new Answer();
+
+            //Act
+            answer.Question = fakeQuestions;
+
+            //Assert
+            Assert.AreSame(fakeQuestions, answer.Question);
+        }
+    }
+}
