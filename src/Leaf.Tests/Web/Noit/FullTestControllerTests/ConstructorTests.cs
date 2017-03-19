8a70d6dbb21dc3d558713e632a77f523cc6c0c85
KristiyanVachev <typhon04@gmail.com>
2017-03-19 10:45:17 +0200

    Added tests for Controllers

---
+++ src/Leaf.Tests/Web/Noit/FullTestControllerTests/ConstructorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Leaf.Services.Contracts;
+using Leaf.Web.Areas.Noit.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Web.Noit.FullTestControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void Constructor_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockFullTestService = new Mock<IFullGameService>();
+
+            // Act
+            var controller = new FullTestController(mockFullTestService.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void Constructor_ShouldThrowNullArgumentException_WhenServiceIsNull()
+        {
+            // Arrange && Act && Assert
+            Assert.Throws<ArgumentNullException>(() => new FullTestController(null));
+        }
+    }
+}
---
+++ src/Leaf.Tests/Web/Noit/FullTestControllerTests/IndexTests.cs
@@ -0,0 +1,28 @@
+﻿using Leaf.Services.Contracts;
+using Leaf.Web.Areas.Noit.Controllers;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Leaf.Tests.Web.Noit.FullTestControllerTests
+{
+    [TestFixture]
+    public class IndexTests
+    {
+        [Test]
+        public void Index_ShouldCallServiceHasUnfinishedTest()
+        {
+            //Arrange
+            var mockFullTestService = new Mock<IFullGameService>();
+
+            //Act
+            var controller = new FullTestController(mockFullTestService.Object);
+
+            //TODO Extract authentication
+            //Assert
+            //controller.WithCallTo(c => c.Index())
+            //    .ShouldRenderView("FullTest");
+
+        }
+    }
+}
