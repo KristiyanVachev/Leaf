c3263e6527f42bd6ede5e70549c5a8fb8fdcdbfd
KristiyanVachev <typhon04@gmail.com>
2017-03-23 22:02:49 +0200

    Added validation for question submission

--- src/Leaf.Web/Areas/Noit/Models/Submit/NewSubmitViewModel.cs
+++ src/Leaf.Web/Areas/Noit/Models/Submit/NewSubmitViewModel.cs
@@ -10,10 +10,16 @@ namespace Leaf.Web.Areas.Noit.Models.Submit
         public string CategoryName { get; set; }
         public IEnumerable<SelectListItem> Categories { get; set; }
 
+        [Required]
+        [MinLength(8, ErrorMessage = "Condition must be atleast 8 symbols long")]
+        [MaxLength(100, ErrorMessage = "Condition must be below 100 symbols long.")]
         public string Condition { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "Correct answer must be below 50 symbols long.")]
         public string CorrectAnswer { get; set; }
 
+        [Required]
         public string[] IncorrectAnswers { get; set; }
     }
 }
