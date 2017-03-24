ca8ee6361ad209e111a697cd4899fc336a414f25
KristiyanVachev <typhon04@gmail.com>
2017-03-24 16:34:31 +0200

    Finished tests for SubmitController

---
+++ src/Leaf.Tests/Web/Noit/SubmitControllerTests/CreateSubmissionTests.cs
@@ -0,0 +1,117 @@
+﻿using Leaf.Auth.Contracts;
+using Leaf.Models;
+using Leaf.Services.Contracts;
+using Leaf.Web.Areas.Noit.Controllers;
+using Leaf.Web.Areas.Noit.Models;
+using Leaf.Web.Areas.Noit.Models.Submit;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace Leaf.Tests.Web.Noit.SubmitControllerTests
+{
+    [TestFixture]
+    public class CreateSubmissionTests
+    {
+        [Test]
+        public void CreateSubmission_ShouldCallAuthenticationServiceCurrentUserId()
+        {
+            // Arrange
+            var mockSubmitService = new Mock<ISubmitService>();
+            var mockAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockViewModelFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubmitController(mockSubmitService.Object,
+                mockAuthenticationProvider.Object,
+                mockViewModelFactory.Object);
+
+            var fakeNewSubmitViewModel = new NewSubmitViewModel();
+
+            var fakeSubmission = new Submission
+            {
+                Id = 0
+            };
+
+            mockSubmitService.Setup(x => x.CreateSubmission(It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>()))
+                .Returns(fakeSubmission);
+                
+            //Act
+            controller.CreateSubmission(fakeNewSubmitViewModel);
+
+            //Assert
+            mockAuthenticationProvider.Verify(x => x.CurrentUserId, Times.Once);
+        }
+
+        [Test]
+        public void CreateSubmission_ShouldCallSubmitServiceCreateSubmission()
+        {
+            // Arrange
+            var mockSubmitService = new Mock<ISubmitService>();
+            var mockAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockViewModelFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubmitController(mockSubmitService.Object,
+                mockAuthenticationProvider.Object,
+                mockViewModelFactory.Object);
+
+            var fakeNewSubmitViewModel = new NewSubmitViewModel();
+
+            var fakeSubmission = new Submission
+            {
+                Id = 0
+            };
+
+            mockSubmitService.Setup(x => x.CreateSubmission(It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>()))
+                .Returns(fakeSubmission);
+
+            //Act
+            controller.CreateSubmission(fakeNewSubmitViewModel);
+
+            //Assert
+            mockSubmitService.Verify(x => x.CreateSubmission(It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>()), Times.Once);
+        }
+
+        [Test]
+        public void CreateSubmissin_ShouldRedirectToSubmission()
+        {
+            // Arrange
+            var mockSubmitService = new Mock<ISubmitService>();
+            var mockAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockViewModelFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubmitController(mockSubmitService.Object,
+                mockAuthenticationProvider.Object,
+                mockViewModelFactory.Object);
+
+            var fakeNewSubmitViewModel = new NewSubmitViewModel();
+
+            var fakeSubmission = new Submission
+            {
+                Id = 0
+            };
+
+            mockSubmitService.Setup(x => x.CreateSubmission(It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string[]>()))
+                .Returns(fakeSubmission);
+
+            //Act
+            controller.WithCallTo(x => x.CreateSubmission(fakeNewSubmitViewModel))
+                .ShouldRedirectTo(x => x.Submission);
+        }
+    }
+}
--- src/Leaf.Tests/Web/Noit/SubmitControllerTests/SubmissionTests.cs
+++ src/Leaf.Tests/Web/Noit/SubmitControllerTests/SubmissionTests.cs
@@ -1,19 +1,21 @@
-﻿using Leaf.Auth.Contracts;
+﻿using System.Collections.Generic;
+using Leaf.Auth.Contracts;
 using Leaf.Models;
 using Leaf.Services.Contracts;
 using Leaf.Web.Areas.Noit.Controllers;
 using Leaf.Web.Areas.Noit.Models;
 using Moq;
 using NUnit.Framework;
+using TestStack.FluentMVCTesting;
 
 namespace Leaf.Tests.Web.Noit.SubmitControllerTests
 {
     [TestFixture]
     public class SubmissionTests
     {
-        //[TestCase(2)]
-        //[TestCase(2342)]
-        public void New_ShouldCallSubmitService_GetCategories(int id)
+        [TestCase(2)]
+        [TestCase(2342)]
+        public void Submission_ShouldCallSubmitService_GetCategories(int id)
         {
             // Arrange
             var mockSubmitService = new Mock<ISubmitService>();
@@ -24,11 +26,49 @@ namespace Leaf.Tests.Web.Noit.SubmitControllerTests
                 mockAuthenticationProvider.Object,
                 mockViewModelFactory.Object);
 
+            var fakeSubmission = new Submission
+            {
+                Category = "",
+                Condition = "",
+                CorrectAnswer = "",
+                IncorrectAnswers = new List<SubmissionAnswer>()
+            };
+
+            mockSubmitService.Setup(x => x.GetSubmissionById(id)).Returns(fakeSubmission);
+
             //Act
             controller.Submission(id);
 
             //Assert
             mockSubmitService.Verify(x => x.GetSubmissionById(id), Times.Once);
+        }
+
+        [TestCase(2)]
+        [TestCase(2342)]
+        public void Submission_ShouldRenderDefaultView(int id)
+        {
+            // Arrange
+            var mockSubmitService = new Mock<ISubmitService>();
+            var mockAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockViewModelFactory = new Mock<IViewModelFactory>();
+
+            var controller = new SubmitController(mockSubmitService.Object,
+                mockAuthenticationProvider.Object,
+                mockViewModelFactory.Object);
+
+            var fakeSubmission = new Submission
+            {
+                Category = "",
+                Condition = "",
+                CorrectAnswer = "",
+                IncorrectAnswers = new List<SubmissionAnswer>()
+            };
+
+            mockSubmitService.Setup(x => x.GetSubmissionById(id)).Returns(fakeSubmission);
+
+
+            //Act && Assert
+            controller.WithCallTo(x => x.Submission(id)).ShouldRenderDefaultView();
         }
     }
 }
