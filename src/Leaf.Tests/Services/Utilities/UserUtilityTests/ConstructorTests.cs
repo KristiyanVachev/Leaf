4bfbda6056e3ff16c53629e8d00b351cb3c542d3
KristiyanVachev <typhon04@gmail.com>
2017-09-24 12:00:31 +0300

    Added tests for UserUtility

---
+++ src/Leaf.Tests/Services/Utilities/UserUtilityTests/ConstructorTests.cs
@@ -0,0 +1,31 @@
+﻿using Leaf.Data.Contracts;
+using Leaf.Factories;
+using Leaf.Models;
+using Leaf.Services.Utilities;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Services.Utilities.UserUtilityTests
+{
+    public class ConstructorTests
+    {
+        [Test]
+        public void Constructor_ShouldNotThrow_WhenParametersNotNull()
+        {
+            //Arrange
+            var mockUserRepository = new Mock<IRepository<User>>();
+            var mockCategoryStatisticsRepository = new Mock<IRepository<CategoryStatistic>>();
+            var mockUserFactory = new Mock<IUserFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            //Act && Assert
+            Assert.DoesNotThrow(() =>
+                new UserUtility(mockUserRepository.Object,
+                    mockCategoryStatisticsRepository.Object,
+                    mockUserFactory.Object,
+                    mockUnitOfWork.Object
+                )
+            );
+        }
+    }
+}
---
+++ src/Leaf.Tests/Services/Utilities/UserUtilityTests/GetByIdTests.cs
@@ -0,0 +1,61 @@
+﻿using Leaf.Data.Contracts;
+using Leaf.Factories;
+using Leaf.Models;
+using Leaf.Services.Utilities;
+using Moq;
+using NUnit.Framework;
+
+namespace Leaf.Tests.Services.Utilities.UserUtilityTests
+{
+    public class GetByIdTests
+    {
+        [TestCase("423rwefs6")]
+        [TestCase("4ruwefsjl-t0ed")]
+        public void GetById_ShouldReturnNull_WhenQuestionIsNotFound(string id)
+        {
+            //Arrange
+            var mockUserRepository = new Mock<IRepository<User>>();
+            var mockCategoryStatisticsRepository = new Mock<IRepository<CategoryStatistic>>();
+            var mockUserFactory = new Mock<IUserFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var utility = new UserUtility(mockUserRepository.Object,
+                mockCategoryStatisticsRepository.Object,
+                mockUserFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+            //Act
+            var user = utility.GetById(id);
+
+            //Assert
+            Assert.IsNull(user);
+        }
+
+        [TestCase("frttgyhujik")]
+        [TestCase("fugihjipok[iuygfugh")]
+        public void GetById_ShouldReturnQuestion_WhenFound(string id)
+        {
+            //Arrange
+            var mockUserRepository = new Mock<IRepository<User>>();
+            var mockCategoryStatisticsRepository = new Mock<IRepository<CategoryStatistic>>();
+            var mockUserFactory = new Mock<IUserFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var utility = new UserUtility(mockUserRepository.Object,
+                mockCategoryStatisticsRepository.Object,
+                mockUserFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+
+            mockUserRepository.Setup(x => x.GetById(id)).Returns(new Mock<User>().Object);
+
+            //Act
+            var question = utility.GetById(id);
+
+            //Assert
+            Assert.IsInstanceOf<User>(question);
+        }
+    }
+}
