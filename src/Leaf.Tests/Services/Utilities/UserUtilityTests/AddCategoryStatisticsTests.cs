53e1f8dde3a2de065f47833f4861881145ff9a9a
KristiyanVachev <typhon04@gmail.com>
2017-09-27 22:17:16 +0300

    Added tests for AddCategoryStatistics

--- src/Leaf.Tests/Services/Utilities/UserUtilityTests/AddCategoryStatisticsTests.cs
+++ src/Leaf.Tests/Services/Utilities/UserUtilityTests/AddCategoryStatisticsTests.cs
@@ -34,34 +34,80 @@ namespace Leaf.Tests.Services.Utilities.UserUtilityTests
             Assert.DoesNotThrow(() => utility.AddCategoryStatistics(id, mockCategoryStatistics.Object));
         }
 
-        //[TestCase("423rwefs6")]
-        //[TestCase("4ruwefsjl-t0ed")]
-        //public void AddCategoryStatistics_Should(string id)
-        //{
-        //    //Arrange
-        //    var mockUserRepository = new Mock<IRepository<User>>();
-        //    var mockCategoryStatisticsRepository = new Mock<IRepository<CategoryStatistic>>();
-        //    var mockUserFactory = new Mock<IUserFactory>();
-        //    var mockUnitOfWork = new Mock<IUnitOfWork>();
+        [Test]
+        public void AddCategoryStatistics_ShouldCreateNewCategoryStatistic_WhenItDoesntExist()
+        {
+            //Arrange
+            var mockUserRepository = new Mock<IRepository<User>>();
+            var mockCategoryStatisticsRepository = new Mock<IRepository<CategoryStatistic>>();
+            var mockUserFactory = new Mock<IUserFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
 
-        //    var utility = new UserUtility(mockUserRepository.Object,
-        //        mockCategoryStatisticsRepository.Object,
-        //        mockUserFactory.Object,
-        //        mockUnitOfWork.Object
-        //    );
+            var utility = new UserUtility(mockUserRepository.Object,
+                mockCategoryStatisticsRepository.Object,
+                mockUserFactory.Object,
+                mockUnitOfWork.Object
+            );
 
-        //    var fakeCategoryStatistics = new List<CategoryStatisticHelper>()
-        //    {
-        //        new CategoryStatisticHelper(0)
-        //    };
+            var fakeCategoryStatisticsHelper = new List<CategoryStatisticHelper>
+            {
+                new CategoryStatisticHelper(0)
+            }.AsEnumerable();
 
-        //    var mock = new Mock<IQueryable<CategoryStatistic>>();
+            var fakeUser = new User();
+            mockUserRepository.Setup(x => x.GetById(It.IsAny<string>())).Returns(fakeUser);
 
-        //    //TODO get a mockable replacement of Entities
-        //    //mockCategoryStatisticsRepository.Setup(x => x.Entities.FirstOrDefault()).Returns(new CategoryStatistic());
+            mockUserFactory.Setup(x => x.CreateCategoryStatistic(It.IsAny<int>())).Returns(new CategoryStatistic());
 
-        //    //Act && Assert
-        //    Assert.DoesNotThrow(() => utility.AddCategoryStatistics(id, fakeCategoryStatistics));
-        //}
+            //var fakeCategoryStatistics = new List<CategoryStatistic>
+            //{
+            //    new CategoryStatistic {UserId = fakeUser.Id, CategoryId = 0}
+            //}.AsQueryable();
+
+            //mockCategoryStatisticsRepository.Setup(x => x.Entities).Returns(fakeCategoryStatistics);
+
+            //Act
+            utility.AddCategoryStatistics(It.IsAny<string>(), fakeCategoryStatisticsHelper);
+            
+            //Assert
+            mockUserFactory.Verify(x => x.CreateCategoryStatistic(It.IsAny<int>()), Times.Once);
+        }
+
+        [Test]
+        public void AddCategoryStatistics_ShouldUpdateCategoryStatistic_WhenItExists()
+        {
+            //Arrange
+            var mockUserRepository = new Mock<IRepository<User>>();
+            var mockCategoryStatisticsRepository = new Mock<IRepository<CategoryStatistic>>();
+            var mockUserFactory = new Mock<IUserFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var utility = new UserUtility(mockUserRepository.Object,
+                mockCategoryStatisticsRepository.Object,
+                mockUserFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+            var fakeCategoryStatisticsHelper = new List<CategoryStatisticHelper>
+            {
+                new CategoryStatisticHelper(0)
+            }.AsEnumerable();
+
+            var fakeUser = new User();
+            mockUserRepository.Setup(x => x.GetById(It.IsAny<string>())).Returns(fakeUser);
+
+            var fakeCategoryStatistics = new List<CategoryStatistic>
+            {
+                new CategoryStatistic {UserId = fakeUser.Id, CategoryId = 0}
+            }.AsQueryable();
+
+            mockCategoryStatisticsRepository.Setup(x => x.Entities).Returns(fakeCategoryStatistics);
+
+            //Act
+            utility.AddCategoryStatistics(It.IsAny<string>(), fakeCategoryStatisticsHelper);
+
+            //Assert
+            mockCategoryStatisticsRepository.Verify(x => x.Update(It.IsAny<CategoryStatistic>()), Times.Once);
+        }
     }
 }
